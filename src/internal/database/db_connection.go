45b61982495be56a6f9f770abdd02d9488ed4678
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-06-28 22:05:29 +0200

    more resilience

--- src/internal/database/db_connection.go
+++ src/internal/database/db_connection.go
@@ -6,18 +6,27 @@ import (
 	"log"
 	"os"
 	"safehouse-main-back/src/internal/models"
+	"time"
 )
 
 func InitDB() *gorm.DB {
 
+	maxRetries := 15 // Retry for 30 seconds (with a 2-second interval)
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		log.Fatal("DATABASE_URL environment variable not set")
 	}
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
+	var db *gorm.DB
+	var err error
+	for i := 0; i < maxRetries; i++ {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			log.Println("Connected to the database successfully!")
+			break
+		}
+		log.Printf("Retrying to connect to the database... attempt %d, error: %v\n", i+1, err)
+		time.Sleep(2 * time.Second)
 	}
 
 	return db
--- src/internal/middleware/security_headers.go
+++ src/internal/middleware/security_headers.go
@@ -14,19 +14,16 @@ func SecurityHeadersMiddleware(config configuration.Config) gin.HandlerFunc {
 
 		c.Header("X-Content-Type-Options", "nosniff")
 		c.Header("Referrer-Policy", "strict-origin-when-cross-origin")
+		c.Header("X-Frame-Options", "DENY")
+		c.Header("X-XSS-Protection", "1; mode=block")
+		c.Header("Cache-Control", "no-store, no-cache, must-revalidate, private")
+		c.Header("Pragma", "no-cache")
+		c.Header("Expires", "0")
 
 		if isSwagger && config.Environment == "development" {
 			c.Header("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';")
-			c.Header("Cache-Control", "no-store, no-cache, must-revalidate, private")
-			c.Header("Pragma", "no-cache")
-			c.Header("Expires", "0")
 		} else {
-			c.Header("X-Frame-Options", "DENY")
-			c.Header("X-XSS-Protection", "1; mode=block")
 			c.Header("Content-Security-Policy", "default-src 'none'; frame-ancestors 'none';")
-			c.Header("Cache-Control", "no-store, no-cache, must-revalidate, private")
-			c.Header("Pragma", "no-cache")
-			c.Header("Expires", "0")
 		}
 
 		c.Next()
